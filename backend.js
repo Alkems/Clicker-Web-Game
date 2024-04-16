7ef29359f5293c4d5e995a792b9ff232843e4246
Alkem <mynamezalter@gmail.com>
2024-04-16 21:55:38 +0300

    Update calculations. Give less BTC from each click.

--- backend.js
+++ backend.js
@@ -1,11 +1,11 @@
     // Beginning values, kind of Globalvariables.
-    var pointsPerClick = 0.0001;
+    var pointsPerClick = 0.000001;
     localStorage.setItem("pointsPerClick", pointsPerClick.toString());
 
     var Total = 0;
     localStorage.setItem("Total", Total.toString());
 
-    upgradeCost = 0.001;
+    upgradeCost = 0.00001;
     localStorage.setItem("upgradeCost", upgradeCost.toString());
 
 
@@ -25,13 +25,13 @@
     // Functions
     // Adds points on each Click me! button hit.
     function addPoints(){
-        var pointsPerClick = parseFloat(localStorage.getItem("pointsPerClick")) || 0.0001;
+        var pointsPerClick = parseFloat(localStorage.getItem("pointsPerClick")) || 0.000001;
 
         var Total = parseFloat(localStorage.getItem("Total")) || 0;
         Total += pointsPerClick;
         localStorage.setItem("Total", Total);
 
-        document.getElementById("points").textContent = "Points: " + Total.toFixed(4);
+        document.getElementById("points").textContent = "Points: " + Total.toFixed(6);
     };
 
     // Upgrades the amount of points you get by each click and requires points to upgrade.
@@ -40,16 +40,16 @@
 
         if (Total > upgradeCost) {
             Total = Total - upgradeCost;
-            localStorage.setItem("Total", Total.toFixed(4));
+            localStorage.setItem("Total", Total.toFixed(6));
 
-            pointsPerClick = pointsPerClick * 1.5;
-            localStorage.setItem("pointsPerClick", pointsPerClick.toFixed(4));
+            pointsPerClick = pointsPerClick * 1.3;
+            localStorage.setItem("pointsPerClick", pointsPerClick.toFixed(6));
 
-            upgradeCost = upgradeCost * 1.3;
-            localStorage.setItem("upgradeCost", upgradeCost.toFixed(4));
+            upgradeCost = upgradeCost * 1.1;
+            localStorage.setItem("upgradeCost", upgradeCost.toFixed(6));
 
-            document.getElementById("points").textContent = "Points: " + Total.toFixed(4);
-            document.getElementById("upgrade").textContent = "Upgrade: " + upgradeCost.toFixed(4) + "BTC!"
+            document.getElementById("points").textContent = "Points: " + Total.toFixed(6);
+            document.getElementById("upgrade").textContent = "Upgrade: " + upgradeCost.toFixed(6) + "BTC!"
         } else {
             alert("You do not have enough points to upgrade.")
         }
